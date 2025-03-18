d7975c74e0468573197f6dd059a15f87f6588b9a
Jonas Kramer <jonkra91@hotmail.com>
2025-03-18 22:38:39 +0100

    added logic for block to block type and tests

--- src/blocks.py
+++ src/blocks.py
@@ -1,5 +1,35 @@
+from enum import Enum
+import re
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
     block_strings = markdown.split("\n\n")
     cleaned_lines = filter(str.strip, block_strings)
     stripped_lines = list(map(str.strip, cleaned_lines))
     return stripped_lines
+
+def is_valid_ordered_list(text):
+    lines = text.strip().split('\n')
+    for i, line in enumerate(lines, start=1):
+        if not re.match(rf'^{i}\. ', line):
+            return False
+    return True
+
+def block_to_block_type(block):
+    if re.match(r'^#{1,6} ', block):
+        return BlockType.HEADING
+    if re.match(r'^```.*```$', block, re.DOTALL):
+        return BlockType.CODE
+    if re.fullmatch(r'(>.*\n?)+', block):
+        return BlockType.QUOTE
+    if re.fullmatch(r'(- .*\n?)+', block):
+        return BlockType.UNORDERED_LIST
+    if is_valid_ordered_list(block):
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from blocks import markdown_to_blocks
+from blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -45,3 +45,27 @@ This is the same paragraph on a new line
             "- This is a list\n- with items",
         ],
         )
+
+    def test_block_to_block_type_heading(self):
+        block = "#### This is a heading block"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+    
+    def test_block_to_block_type_code(self):
+        block = "````This is a code block```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+
+    def test_block_to_block_type_quote(self):
+        block = ">This is a quote\n>This is a new line"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+    
+    def test_block_to_block_type_ulist(self):
+        block = "- This is the first line\n- This is the second\n- This is the third"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+
+    def test_block_to_block_type_olist(self):
+        block = "1. This is the first line\n2. This is the second\n3. This is the third"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+    
+    def test_block_to_block_type_paragraph(self):
+        block = "This is a normal paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
