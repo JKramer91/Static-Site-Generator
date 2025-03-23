d309d86bc1927be237bdfc0d54d5e0bb6602b196
Jonas Kramer <jonkra91@hotmail.com>
2025-03-23 14:39:03 +0100

    cleanup

--- src/main.py
+++ src/main.py
@@ -5,14 +5,15 @@ from copy_directory import source_to_destination
 from generate import generate_page, generate_pages_recursive
 
 static_path = "./static"
-public_path = "./public"
+public_path = "./docs"
+content_path = "./content"
+template_path = "./template.html"
+default_path = "/"
 
 def main():
-    basepath = ""
-    if sys.argv[0]:
-        basepath = sys.argv[0]
-    else:
-        basepath = "/"
+    basepath = default_path
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
     if os.path.exists(public_path):
         shutil.rmtree(public_path)
     os.mkdir(public_path)
